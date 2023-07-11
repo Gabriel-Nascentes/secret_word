ed7c65e79dfc4466f809d01eaa7bb1e555134187
Gabriel Nascentes <gno.gabriel@gmail.com>
2023-07-10 23:00:46 -0300

    Enviando projeto para deploy.

---
+++ src/components/EndScreen.js
@@ -0,0 +1,16 @@
+import "./EndScreen.css";
+
+const EndScreen = ({ retry, score }) => {
+  return (
+    <div>
+      {" "}
+      <h1>Fim de jogo!</h1>
+      <h2>
+        A sua pontuação foi: <span>{score}</span>
+      </h2>
+      <button onClick={retry}>Resetar Jogo</button>
+    </div>
+  );
+};
+
+export default EndScreen;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>© 2023 - Desenvolvido por Gabriel Nascentes.</p>
+    </div>
+  );
+};
+
+export default Footer;
